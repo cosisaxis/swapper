73fcd9793a24db2e5e43e9fd35c6d6389037b03e
cosisaxis <kishanelewis2@gmail.com>
2022-08-29 17:59:08 -0500

    modal

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-async function connection(){
-    if(typeof window !== "undefined"){
-        await ethereum.request({ method: 'eth_requestAccounts'});
-        document.getElementById("login_button").innerHTML= "Connected";
-        } else {
-            document.getElementById("login_button").innerHTML = "Install MetaMask"
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+          console.log("connecting");
+          await ethereum.request({ method: "eth_requestAccounts" });
+        } catch (error) {
+          console.log(error);
         }
-}
-
-document.getElementById("login_button").onclick = connection;
+        document.getElementById("login_button").innerHTML = "Connected";
+        // const accounts = await ethereum.request({ method: "eth_accounts" });
+        document.getElementById("swap_button").disabled = false;
+      } else {
+        document.getElementById("login_button").innerHTML =
+          "Please install MetaMask";
+      }
+    }
+  
+  function openModal(){
+    document.getElementById("token_modal").style.display = "block";
+  }
+  
+  function closeModal(){
+  document.getElementById("token_modal").style.display = "none";
+  }
+  
+  document.getElementById("login_button").onclick = connect;
+  document.getElementById("from_token_select").onclick = openModal;
+  document.getElementById("modal_close").onclick = closeModal;
